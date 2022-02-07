165a2c7628066726d82d62ee78f4ef9d6f0a394e
vvenukaushik <85499191+vvenukaushik@users.noreply.github.com>
2022-02-07 18:37:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+function playAudio(event) {
+
+    switch (event) {
+        case 'w':
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+        case 'l':
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+        default:
+            console.log("NO sound found");
+    }
+    
+
+}
+
+var len = document.querySelectorAll(".drum").length;
+for (var i =0; i<len; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var buttonClicked = this.innerHTML;
+        playAudio(buttonClicked);
+        buttonAnimation(buttonClicked);
+    });
+
+}
+
+document.addEventListener("keypress",function(eve) {
+    playAudio(eve.key);
+    buttonAnimation(eve.key);
+});
+
+function buttonAnimation(clicked) {
+    var buttonPressed = document.querySelector("." + clicked).classList;
+    buttonPressed.add("pressed");
+    setTimeout( function () {
+        buttonPressed.remove("pressed");
+    }, 100)
+}
